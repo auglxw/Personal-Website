3066701bd1e842fb771e3b8f7acb999adadda680
Augustine Lee <62803914+auglxw@users.noreply.github.com>
2022-06-21 23:38:02 +0800

    Edit About component
    Signed-off-by: Augustine Lee <62803914+auglxw@users.noreply.github.com>

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -4,14 +4,21 @@ import {Container, Navbar, Nav, Row, Col} from 'react-bootstrap';
 function AboutBody() {
     return <Container fluid className="about">
     <Row className="aboutGrid">
-        <Col lg={4} xs={12} className="aboutPicture">
+        <Col lg={4} xs={12}>
             <div>
-                <h3>Picture</h3>
+                <img className="aboutPicture" src={require("../img/profilepic.png")} alt="Profile Pic" height="500rem" />
             </div>
         </Col>
-        <Col lg={8} xs={12}>
-            <h1 className="aboutTitle">HELLO THERE!</h1>
-            <p className="aboutContent">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <Col lg={8} xs={12} className="aboutWords">
+            <div className="aboutTitleGroup">
+                <p className="aboutTitle">HELLO THERE!</p>
+            </div>
+            <p className="aboutContent">I am currently an undergraduate at Nanyang Technological University, majoring in Computer Science and Business. As a self-motivated and conscientious individual, I am constantly learning new technologies to broaden my skill set. I aspire to be able to optimise and improve business processes through the use of technology. I am familiar with the following technologies:</p>
+            <p className="aboutContent">
+            <b>Programming Languages</b>: Python, C Programming, JavaScript, SQL<br />
+            <b>Web Development</b>: HTML, CSS (& Bootstrap), JavaScript ES6, React.JS, Node.JS, Express.JS<br />
+            <b>Data Science</b>: Numpy, Pandas, MatPlotLib, Seaborn, Scikit-Learn, XGBoost
+            </p>
         </Col>
     </Row>
 </Container>
